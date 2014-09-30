8c236c6d722c393629046edd190df94628170672
myron0815@gmx.net <myron0815@gmx.net@5295703c-e1a7-429d-2eee-165e1990fda6>
2014-09-30 16:10:57 +0000

    merge r1662 - r1680

--- tinyMediaManager/src/org/tinymediamanager/core/Utils.java
+++ tinyMediaManager/src/org/tinymediamanager/core/Utils.java
@@ -56,6 +56,7 @@ import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tinymediamanager.Globals;
 import org.tinymediamanager.LaunchUtil;
 import org.tinymediamanager.ReleaseInfo;
 import org.tinymediamanager.core.Message.MessageLevel;
@@ -393,7 +394,7 @@ public class Utils {
                 + "&tid=UA-35564534-5"
                 + "&cid=" + uuid 
                 + "&an=tinyMediaManager" 
-                + "&av=" + ReleaseInfo.getBuild() 
+                + "&av=" + ReleaseInfo.getBuild() // svn revision number (or nightly/prerel)
                 + "&t=event"
                 + "&ec=" + event
                 + "&ea=" + event 
@@ -404,7 +405,9 @@ public class Utils {
                 + "&sr=" + java.awt.Toolkit.getDefaultToolkit().getScreenSize().width + "x" + java.awt.Toolkit.getDefaultToolkit().getScreenSize().height 
                 + "&cd1=" + getEncProp("os.name") 
                 + "&cd2=" + getEncProp("os.arch") 
-                + "&cd3=" + getEncProp("java.version") 
+                + "&cd3=" + getEncProp("java.specification.version") // short; eg 1.7
+                + "&cd4=" + ReleaseInfo.getVersion() // TMM version eg 2.5.5
+                + "&cd5=" + (Globals.isDonator() ? "1" : "0")
                 + "&z=" + System.currentTimeMillis();
             // @formatter:on
             Url url = new Url("http://www.google-analytics.com/collect?" + ga);
